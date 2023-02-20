ff3767f7708e4db672797cbdd4e1b4629497d8a7
Adem ilter <ademilter@gmail.com>
2023-02-21 01:11:37 +0300

    fix: key name

--- components/list.tsx
+++ components/list.tsx
@@ -6,12 +6,12 @@ import Row, { RowProps } from "@/components/row";
 export default function List() {
   const [data, setData] = React.useState<{
     lastUpdate: string;
-    last100: RowProps[];
+    data: RowProps[];
   }>();
 
   const fetchData = async () => {
     const res = await fetch("/api");
-    const data = await res.json();
+    const { data } = await res.json();
     if (res.ok) {
       setData(data);
     }
@@ -24,7 +24,7 @@ export default function List() {
 
   return (
     <div className="space-y-0.5">
-      {data?.last100.map((row) => (
+      {data?.data.map((row) => (
         <Row key={row.id} {...row} />
       ))}
     </div>
