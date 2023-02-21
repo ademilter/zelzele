c05697aac243bb24d37a0c0b9e71fb4beb6792d2
imhalid <imhalid@icloud.com>
2023-02-21 19:14:41 +0300

    fix: property typo

--- components/filter.tsx
+++ components/filter.tsx
@@ -37,11 +37,11 @@ export default function Filter({ filter, setFilter, fetchData }: AllFilterProps)
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="#ffffff"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round">
+          strokeLinecap="round"
+          strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
           <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
