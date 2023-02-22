a4c58f3b8ed211ea1d5afc127f8e6d8ce70ed665
Semih Zambak <semih12zambak@gmail.com>
2023-02-22 13:33:35 +0300

    fix: theme icon

--- components/filter.tsx
+++ components/filter.tsx
@@ -52,14 +52,15 @@ export default function Filter({
               theme ? (
                 <>
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
+                  <circle cx="12" cy="12" r="4"/>
+                  <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"/>
                 </>
               ) : (
                 <>
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                  <circle cx="12" cy="12" r="4"/>
-                  <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"/>
-                </>)
+                  <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
+                </>
+              )
             }
           </svg>
 
