ee40be358d833d50e527221a017fc256438eb1aa
emirhanwsd <leetpocketmine@gmail.com>
2023-08-10 23:18:03 +0300

    Change row colors for dark mode

--- components/row.tsx
+++ components/row.tsx
@@ -10,9 +10,9 @@ export default function Row({ item }: Props) {
   const { setFilter, filter } = store();
 
   const styleContainer = {
-    "1": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-white", // 1-1,9
-    "2": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-white", // 2-2,9
-    "3": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-white", // 3-3,9
+    "1": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-zinc-100", // 1-1,9
+    "2": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-zinc-100", // 2-2,9
+    "3": "from-zinc-100 dark:from-zinc-900 bg-gradient-to-l text-zinc-900 dark:text-zinc-100", // 3-3,9
     "4": "from-orange-100 dark:from-orange-900 bg-gradient-to-l text-orange-900 dark:text-orange-100", // 4-4,9
     "5": "from-pink-100 dark:from-pink-900 bg-gradient-to-l text-pink-900 dark:text-pink-100", // 5-5,9
     "6": "from-red-100 dark:from-red-900 bg-gradient-to-l text-red-900 dark:text-red-100", // 6-6,9
